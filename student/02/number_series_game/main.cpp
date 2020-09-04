5b2f340d6cd71aa954ad5c4fe7948ff450c4fd03
Asbah Amjad Usmani <khasus@linux-desktop9.tuni.fi>
2020-09-04 16:39:43 +0300

    both 3 and 7

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -8,9 +8,17 @@ int main()
     cin >> num;
     for(int i=1; i<=num; i++)
     {
-        if(i%3==0)
+        if(i%3==0 && i%7==0)
         {
-           cout << "zip" << endl;
+           cout << "zip boing" << endl;
+        }
+        else if(i%7==0)
+        {
+        cout << "boing" << endl;
+        }
+        else if(i%3==0)
+        {
+        cout << "zip" << endl;
         }
         else
         {
