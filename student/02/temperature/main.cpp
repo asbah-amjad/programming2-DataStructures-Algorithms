d727da519e73004118447271fc584a26c426aa43
Asbah Amjad Usmani <khasus@linux-desktop9.tuni.fi>
2020-09-04 15:50:47 +0300

    Final commit

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main()
 {
+    int temp = 0;
     cout << "Enter a temperature: ";
+    cin >> temp;
+    double F = (temp * 1.8) + 32;
+    double C = (temp - 32) / 1.8;
 
-    // Write your code here
+    cout << temp << " degrees Celsius is " << F <<  " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << C <<  " degrees Celsius" << endl;
 
     return 0;
 }
