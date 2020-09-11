a5ec65d96e3573e0f61e31b75ac846f08f855131
Asbah Amjad Usmani <khasus@linux-desktop9.tuni.fi>
2020-09-11 20:06:58 +0300

    error checks

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 #include<string>
 #include<cctype>
-
+#include <bits/stdc++.h>
 using namespace std;
+
+bool checkPangram(string str)
+{
+    // Create a hash table to mark the characters
+    // present in the string
+    vector<bool> mark(26, false);
+
+    // For indexing in mark[]
+    int index;
+
+    // Traverse all characters
+    for (int i = 0; i < str.length(); i++) {
+
+        // If lowercase character, subtract 'a'
+        // to find index.
+        if ('a' <= str[i] && str[i] <= 'z')
+            index = str[i] - 'a';
+
+        // If this character is other than english
+        // lowercase and uppercase characters.
+        else
+            continue;
+
+        mark[index] = true;
+    }
+
+    // Return false if any character is unmarked
+    for (int i = 0; i <= 25; i++)
+        if (mark[i] == false)
+            return (false);
+
+    // If all characters were present
+    return (true);
+}
 
 int main(){
 
@@ -18,7 +52,7 @@ int main(){
         cout << "Error! The encryption key must contain 26 characters.";
         return EXIT_FAILURE;
     }
-
+/*
     for(int i=0; i<len;i++){
         int ascii= int(key[i]);
         if(!(ascii>=97 && ascii<=122)){
@@ -27,13 +61,18 @@ int main(){
         }
 
     }
-
+*/
     for(int i=0; i<len;i++){
         char_array[i] = key[i];
         if(!islower(char_array[i])){
             cout << "Error! The encryption key must contain only lower case characters.";
             return EXIT_FAILURE;
         }
+    }
+    
+    if(checkPangram(key)==false){
+        cout << "Error! The encryption key must contain all alphabets a-z.";
+        return EXIT_FAILURE;
     }
 
     string encrypt="";
