b826172a45260c18ee801770690288b06ed842d5
Asbah Amjad Usmani <khasus@linux-desktop10.tuni.fi>
2020-09-11 15:00:09 +0300

    testing

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
 using namespace std;
-unsigned long int factorial(unsigned long int n);
-unsigned long int combinations(unsigned long int l, unsigned long d);
+long int factorial(long int n);
+long int combinations(long int l, long d);
 
 int main()
 {
-   unsigned long int lottery_balls=0;
-   unsigned long int drawn_balls=0;
+   long int lottery_balls=0;
+   long int drawn_balls=0;
 
    cout << "Enter the total number of lottery balls: ";
    cin >> lottery_balls;
    cout << "Enter the number of drawn balls: ";
    cin >> drawn_balls;
-
+//number of balls must be a positive number
    if(lottery_balls >=0 && drawn_balls >=0){
        if(drawn_balls > lottery_balls){
                   cout << "The maximum number of drawn balls is the total amount of balls.";
               }
               else{
-                  unsigned long int result= 1/combinations(lottery_balls,drawn_balls);
+                  long int result= combinations(lottery_balls,drawn_balls);
 
                   cout <<"The probability of guessing all " << drawn_balls <<" balls correctly is " << result;
               }
@@ -30,17 +30,22 @@ int main()
 
 }
 
-unsigned long int factorial(unsigned long int n){
+long int factorial(long int n){
     int fact =1;
-    for(unsigned long int i=1; i<=n; i++){
+    if(n==0 or n==1){
+        return fact;
+    }
+    else{
+    for(long int i=1; i<=n; i++){
         fact = fact*i;
     }
     return fact;
+    }
 }
 
-unsigned long int combinations(unsigned long int l, unsigned long d){
-    unsigned long int r = l-d;
-    unsigned long int a = factorial(l);
-    unsigned long int b = factorial(r) * factorial(d);
+long int combinations(long int l, long d){
+     long int r = l-d;
+     long int a = factorial(l);
+     long int b = factorial(r) * factorial(d);
     return a/b;
 }
