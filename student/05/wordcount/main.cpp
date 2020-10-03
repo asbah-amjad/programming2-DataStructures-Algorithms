ec1deaa21bb20aa0491081103296150eafa525ac
Asbah Amjad Usmani <khasus@linux-desktop5.tuni.fi>
2020-10-03 22:18:14 +0300

    try

--- student/05/wordcount/main.cpp
+++ student/05/wordcount/main.cpp
@@ -4,11 +4,12 @@
 #include <map>
 #include <sstream>
 #include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int line_number=0;
-    int count = 0;
+    int word_count = 0;
     map<string, string> words;
     string inputFile = "";
     cout << "Input file: ";
@@ -18,18 +19,38 @@ int main(){
     if(file_object){
 
         string line = "";
-        string word;
+
         while(getline(file_object, line, '\n')){
             line_number = 1;
+            vector<string> p;
+            string temp;
+            stringstream ss(line);
+            while(getline(ss, temp, ' ')){
 
-            if(line.find(word) != string::npos){
-                count++;
+                if(find(p.begin(),p.end(),temp) == p.end()){
+                p.push_back(temp);
+                }
+
+            }
+            for(auto it=begin(p); it!=end(p); ++it){
+
+                if(words.find(*it)==words.end()){
+                    words.insert({*it,to_string(line_number)});
+                    word_count = 1;
+                }
+                else{
+                    string add = ", "+to_string(line_number);
+                    words.at(*it) += add;
+                    word_count++;
+                }
             }
 
             line_number++;
         }
 
-
+        for(auto result : words){
+            cout << result.first << " " << word_count<< ": " << result.second << endl;
+        }
         file_object.close();
         return EXIT_SUCCESS;
     }
