cc2b4a60d2b4ca8f933ba2f655a46057f08610ca
Asbah Amjad Usmani <khasus@linux-desktop8.tuni.fi>
2020-10-11 16:54:57 +0300

    recursive call

--- student/06/vertical/main.cpp
+++ student/06/vertical/main.cpp
@@ -11,25 +11,15 @@ void print_vertical(unsigned int num)
     // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
     // ------------
 
-               if (num/10 > 0) //no need have else blocks
-               {
-                   //for the correct order, make the recursive call first
-                   print_vertical(num/10);
-               }
-               //print when you reach the most significant digit
-               cout << num%10 << endl;
-/*
-    // Add your implementation here
-    while(num > 0){
 
-        digit = num%10;
-        num /= 10;
-        array[i] = digit;
-        i++;
-    }
-    for(int j=i-1; j>=0; j--){
-        cout << array[j] << endl;
-    }*/
+   // Add your implementation here
+            if (num/10 > 0) //no need have else blocks
+            {
+                //for the correct order, make the recursive call first
+                print_vertical(num/10);
+            }
+            //print when you reach the most significant digit
+            cout << num%10 << endl;
 }
 
 // Do not modify rest of the code, or the automated testing won't work.
